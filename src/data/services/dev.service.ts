9d46aa6b9614d7574be0c839447bde80484fd390
alanbrunoscience <alanbrunomath@gmail.com>
2025-02-03 22:06:38 -0300

    :sparkles: Deployment method update

--- src/data/services/dev.service.ts
+++ src/data/services/dev.service.ts
@@ -13,7 +13,7 @@ export class DevService implements TypeOrmOptionsFactory {
       port: 3306,
       username: 'root',
       password: 'root',
-      database: 'db_blogpessoal',
+      database: 'db_personal_blog',
       entities: [Postagem, Tema, Usuario],
       synchronize: true,
     };
